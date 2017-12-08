fe17de3dc449850ea8a833d82ef5a2c07cc94c35
VincentK <vincent.kesumo@gmail.com>
2017-12-08 10:52:45 +0100

    Added an Ads component

---
+++ src/components/Ads.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+class Ads extends React.Component {
+    render() {
+        return (
+            <div id='Ads'>
+                <img src=""/>
+                <p> Insert your Ads here! </p>
+            </div>
+        )
+    }
+}
+
+export default Ads;
