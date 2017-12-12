5747a0fc3995baf6f0605cf2d765528c342af2a6
arne <arne@mintlab.nl>
2017-12-12 08:50:10 +0100

    Remove unnecessary mediator component

--- src/index.js
+++ src/index.js
@@ -3,14 +3,39 @@ import { render } from 'react-dom';
 import { BrowserRouter as Router } from 'react-router-dom';
 
 import './styles/css/style.css';
-import KingPongSuper from './components/KingPongSuper';
+import KingPong from './components/KingPong';
 
-const Root = () => {
-    return (
-        <Router>
-            <KingPongSuper />
-        </Router>
-    )
+class Root extends React.Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            session: false
+        };
+    }
+
+    componentDidMount() {
+        return fetch('/api/user/hash/', {
+            method: "POST",
+            body: JSON.stringify({hash: localStorage.getItem('hash')}),
+            headers: {
+                "Content-Type": "application/json"
+            }
+        })
+        .then(results => {
+            if (results.status === 201) {
+                this.setState({session: true})
+            }
+        });
+    }
+
+    render() {
+        return (
+            <Router>
+                <KingPong session={this.state.session}/>
+            </Router>
+        )
+    }
 }
 
 render(<Root/>, document.querySelector('#main'));
