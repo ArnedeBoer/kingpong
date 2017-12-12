f243c4a6b03ee71902b585ee9c80dbe10a8ca119
arne <arne@mintlab.nl>
2017-12-12 09:27:59 +0100

    Add alt to ad image

--- src/components/Ads.js
+++ src/components/Ads.js
@@ -4,7 +4,7 @@ class Ads extends React.Component {
     render() {
         return (
             <div id='Ads'>
-                <img src=""/>
+                <img src="" alt="ad"/>
                 <p> Insert your Ads here! </p>
             </div>
         )
